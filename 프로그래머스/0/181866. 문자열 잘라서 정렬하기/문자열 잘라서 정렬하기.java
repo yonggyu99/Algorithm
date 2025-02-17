b7f89085ac44fa3944e436d21d42a6319debc6e7
박용규 <yoyo5411@naver.com>
2025-02-17 14:21:15 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 57.58 ms, Memory: 95.5 MB -BaekjoonHub

--- 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
@@ -1,31 +1,21 @@
 import java.util.Arrays;
-
 class Solution {
     public String[] solution(String myString) {
         String[] splitArr = myString.split("x");
-        
         int size = 0;
-        
         for(String s : splitArr){
             if(!s.isEmpty()){
                 size ++;
             }
-            
         }
-        
-        
         String[] answer = new String[size];
-        
         int index = 0;
-        
         for(String s : splitArr){
             if(!s.isEmpty()){
                 answer[index++] = s;
             }
         }
-        
         Arrays.sort(answer);
-        
         return answer;
     }
 }
