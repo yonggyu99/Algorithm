9e390322995ba33947a5556480cf8c2a3c0d18dc
박용규 <yoyo5411@naver.com>
2025-02-18 15:45:32 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 12.69 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        String a = my_string.substring(0, s);
+        String b = my_string.substring(s + overwrite_string.length());
+        return a + overwrite_string + b;
+    }
+}
