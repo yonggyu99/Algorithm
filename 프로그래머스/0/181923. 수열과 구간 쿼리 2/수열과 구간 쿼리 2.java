e6a9353a21c5d4a8ced4d3355156ad0e738c065c
박용규 <yoyo5411@naver.com>
2025-02-18 16:01:43 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 2.54 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            int s = queries[i][0];
+            int e = queries[i][1];
+            int k = queries[i][2];
+            int min = Integer.MAX_VALUE;
+            boolean found = false;
+            for (int j = s; j <= e; j++) {
+                if (arr[j] > k && arr[j] < min) {
+                    min = arr[j];
+                    found = true;
+                }
+            }
+            answer[i] = found ? min : -1;
+        }
+        return answer;
+    }
+}
