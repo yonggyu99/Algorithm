9352e43707e70e2ff822257fdde09bf0dd5d4d66
박용규 <yoyo5411@naver.com>
2025-02-18 19:22:05 +0900

    [level 0] Title: OX퀴즈, Time: 0.25 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] quiz) {
+        String[] answer = new String[quiz.length];
+
+        for (int i = 0; i < quiz.length; i++) {
+            String[] parts = quiz[i].split(" ");
+
+            int X = Integer.parseInt(parts[0]);
+            String operator = parts[1];
+            int Y = Integer.parseInt(parts[2]);
+            int Z = Integer.parseInt(parts[4]);
+
+            int result = operator.equals("+") ? (X + Y) : (X - Y);
+
+            answer[i] = (result == Z) ? "O" : "X";
+        }
+
+        return answer;
+    }
+}
