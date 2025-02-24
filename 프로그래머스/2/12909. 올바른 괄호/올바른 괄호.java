52fb9cb1c02a53a516b32e83a6094f034858d3a0
박용규 <yoyo5411@naver.com>
2025-02-24 16:08:16 +0900

    [level 2] Title: 올바른 괄호, Time: 5.91 ms, Memory: 54.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,21 @@
+class Solution {
+    boolean solution(String s) {
+        int count = 0;
+        
+        for(char c : s.toCharArray()){
+            if(c == '('){
+                count ++;
+            }
+            else{
+                count --;
+            }
+            
+            if (count<0){
+                return false;
+            }
+        }
+
+
+        return count == 0;
+    }
+}
