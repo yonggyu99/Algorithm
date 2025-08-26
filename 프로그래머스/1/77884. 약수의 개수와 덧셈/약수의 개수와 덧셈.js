56e8d77e03c3ffbd5b01ad1120efa3a8b83d62a9
yonggyu <yoyo5411@naver.com>
2025-08-26 19:22:17 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 29.92 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -0,0 +1,21 @@
+function solution(left, right) {
+    var answer = 0;
+    var arr = [];
+    for(let i = left; i<=right; i++){
+        for(let j = 1; j <= i; j++){
+            if(i%j ===0){
+                arr.push(j)
+            }
+        }
+        if(arr.length % 2 ===0){
+            answer+= i;
+        }
+        else{
+            answer-=i;
+        }
+        
+        arr = [];
+    }
+    
+    return answer;
+}
