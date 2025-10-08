5bc6a688c17424eecdff3a40af8331996f0fb5a2
yonggyu <yoyo5411@naver.com>
2025-10-08 20:47:14 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.24 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,13 +1,4 @@
 function solution(n, arr1, arr2) {
-  const result = [];
-
-  for (let i = 0; i < n; i++) {
-    const line = arr1[i] | arr2[i];
-    const binary = line.toString(2);
-    const padded = binary.padStart(n, "0");
-    const row = padded.replace(/1/g, "#").replace(/0/g, " ");
-    result.push(row);
-  }
-
-  return result;
+    var answer = [];
+    return answer;
 }
