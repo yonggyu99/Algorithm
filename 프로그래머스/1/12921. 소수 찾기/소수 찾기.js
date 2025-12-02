d2a45e8d584068cc768d83db9e71f7f6ba4d0ef5
yonggyu <yoyo5411@naver.com>
2025-12-02 16:56:02 +0900

    [level 1] Title: 소수 찾기, Time: 52.79 ms, Memory: 44.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -0,0 +1,15 @@
+function solution(n) {
+    const isPrime = Array(n + 1).fill(true);
+    
+    isPrime[0] = false;
+    isPrime[1] = false;
+    
+    for(let i = 2; i * i <= n; i++){
+        if(isPrime[i]){
+            for(let j = i *i ; j <= n; j+= i){
+                isPrime[j] = false;
+            }
+        }
+    }
+    return isPrime.filter((v) => v).length;
+}
