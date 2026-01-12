58283ca03aa7f5b8149cbe005f2cb28d0660d997
yonggyu <yoyo5411@naver.com>
2026-01-12 20:26:31 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.78 ms, Memory: 32.9 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,13 +1,11 @@
 function solution(A,B){
     var answer = 0;
     
-    A.sort((a,b) => a-b);
-    B.sort((a,b) => b-a);
+    A = A.sort((a, b) => a - b);
+    B = B.sort((a, b) => b - a);
     
-    for(var i = 0; i<A.length; i ++){
-        answer += A[i]*B[i]
-    }
-    answer
-    
+    for(let i = 0; i < A.length; i++){
+    answer = answer + A[i] * B[i]
+        }
     return answer;
 }
