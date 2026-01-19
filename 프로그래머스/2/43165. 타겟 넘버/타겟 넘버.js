b42b4979f57f4eadcb959aa975b9fabbaa4a99e9
yonggyu <yoyo5411@naver.com>
2026-01-19 20:02:54 +0900

    [level 2] Title: 타겟 넘버, Time: 24.28 ms, Memory: 35.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -0,0 +1,18 @@
+function solution(numbers, target) {
+    var count = 0;
+    
+    function dfs(idx, sum){
+        if(idx === numbers.length){
+            if(sum === target){
+                count++;
+            }
+            return;
+        }
+        
+        dfs(idx + 1, sum + numbers[idx]);
+        dfs(idx + 1, sum - numbers[idx]);
+    }
+    
+    dfs(0, 0);
+    return count;
+}
