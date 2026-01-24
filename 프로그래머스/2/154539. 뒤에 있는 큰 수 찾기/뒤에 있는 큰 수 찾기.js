f884cd49120e90bbbf996ff603f586e7c3eda704
yonggyu <yoyo5411@naver.com>
2026-01-24 16:47:49 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 60.12 ms, Memory: 152 MB -BaekjoonHub

---
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
@@ -0,0 +1,16 @@
+function solution(numbers) {
+    let answer = new Array(numbers.length).fill(-1);
+    const stack = [];
+    
+    for(let i = 0; i < numbers.length; i++){
+        while(
+        stack.length > 0 &&
+        numbers[stack[stack.length - 1]] < numbers[i])
+        {
+        const index = stack.pop();
+        answer[index] = numbers[i];
+        }
+        stack.push(i);
+    }
+    return answer;
+}
