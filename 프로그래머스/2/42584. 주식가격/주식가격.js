6464fdab77d68e1e043c4f708a5e0180e0bba67f
yonggyu <yoyo5411@naver.com>
2026-01-29 15:29:12 +0900

    [level 2] Title: 주식가격, Time: 6.01 ms, Memory: 45.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -0,0 +1,12 @@
+function solution(prices) {
+    var answer = [];
+    for(let i = 0; i < prices.length; i++){
+        let time = 0;
+        for(let j = i+1; j < prices.length; j++){
+            time ++;
+            if(prices[i] > prices[j]) break;
+        }
+        answer.push(time);
+    }
+    return answer;
+}
