a96ff3497e6fd3b9c439660470e3b8c61971642f
yonggyu <yoyo5411@naver.com>
2026-01-30 19:50:28 +0900

    [level 2] Title: 땅따먹기, Time: 48.78 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.js
@@ -0,0 +1,12 @@
+function solution(land) {
+    const n = land.length;
+
+    for (let i = 1; i < n; i++) {
+        land[i][0] += Math.max(land[i - 1][1], land[i - 1][2], land[i - 1][3]);
+        land[i][1] += Math.max(land[i - 1][0], land[i - 1][2], land[i - 1][3]);
+        land[i][2] += Math.max(land[i - 1][0], land[i - 1][1], land[i - 1][3]);
+        land[i][3] += Math.max(land[i - 1][0], land[i - 1][1], land[i - 1][2]);
+    }
+
+    return Math.max(...land[n - 1]);
+}
