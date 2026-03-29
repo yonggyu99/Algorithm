174be10d8e4126fa16e57f97afe8dd021c21cf96
yonggyu <yoyo5411@naver.com>
2026-03-29 15:56:38 +0900

    [level 2] Title: 의상, Time: 3.32 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,14 +1,15 @@
 function solution(clothes) {
-    const countByType = {};
-    
-    for (const [name, type] of clothes){
-        countByType[type] = (countByType[type] || 0) + 1;
-    }
-    
+    let type = {};
     let answer = 1;
     
-    for(const type in countByType){
-        answer *= (countByType[type] + 1);
+    for(const [cloth, clothType] of clothes){
+        type[clothType] = (type[clothType] || 0) + 1;
     }
-    return answer - 1;;
+    
+    for(const cloth in type){
+        answer *= (type[cloth] + 1)
+    }
+    
+    console.log(type)
+    return answer - 1;
 }
