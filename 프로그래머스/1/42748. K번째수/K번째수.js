a6c50e9389b46bcf99b766eb90143200b9f58705
yonggyu <yoyo5411@naver.com>
2026-03-30 19:55:22 +0900

    [level 1] Title: K번째수, Time: 3.59 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,13 +1,15 @@
 function solution(array, commands) {
     var answer = [];
-    var second_answer = [];
-    var number = 0;
-    for(var i = 0; i<commands.length; i++){
-        second_answer = array.slice(commands[i][0]-1,commands[i][1])
-        second_answer.sort((a,b) => a-b)
-        number = second_answer[commands[i][2]-1];
-        answer.push(number)
+    
+    for(let i = 0; i < commands.length; i++){
+        
+        const newArr = array.slice(commands[i][0] - 1, commands[i][1]);
+        
+        answer.push(newArr.sort((a, b) => a -b)[commands[i][2] - 1])
+        console.log(newArr)
     }
+    
+    
     
     return answer;
 }
