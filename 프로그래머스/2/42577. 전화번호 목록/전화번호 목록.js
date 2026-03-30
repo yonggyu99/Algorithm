dec080905d92257bd0404a2fdef26bee91f83bf0
yonggyu <yoyo5411@naver.com>
2026-03-30 11:16:55 +0900

    [level 2] Title: 전화번호 목록, Time: 542.03 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,16 +1,17 @@
 function solution(phone_book) {
-    const numObj = {};
     
-    for(const number of phone_book){
-        numObj[number] = true;
+    let phoneNumber = {};
+    
+    for(const phone of phone_book){
+        phoneNumber[phone] = true;
     }
     
-    for(const number of phone_book){
+    for(const number in phoneNumber){
         let prefix = "";
         for(let i = 0; i < number.length - 1; i++){
             prefix += number[i];
             
-            if(numObj[prefix]) return false;
+            if(phoneNumber[prefix]) return false;
         }
     }
     return true;
