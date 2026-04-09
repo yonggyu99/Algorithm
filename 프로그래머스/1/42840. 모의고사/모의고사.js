ab39422e166fdbca13e5722715ce261120f2f515
yonggyu <yoyo5411@naver.com>
2026-04-09 22:32:02 +0900

    [level 1] Title: 모의고사, Time: 30.77 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,26 +1,27 @@
 function solution(answers) {
-    var answer = [];
-    const people = [[1, 2, 3, 4, 5], [2, 1, 2, 3, 2, 4, 2, 5], [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]];
+    const answer = [];
+    
+    const patterns = [
+        [1, 2, 3, 4, 5],
+        [2, 1, 2, 3, 2, 4, 2, 5],
+        [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    ];
 
-    const answerCount = new Array(people.length).fill(0);
-    
-    for(let i = 0; i < people.length; i++){
-        for(let j = 0; j < answers.length; j++){
-            if(people[i][j % people[i].length] === answers[j]){
-                answerCount[i]++;
-            };
+    const scores = [0, 0, 0];
+
+    for (let i = 0; i < answers.length; i++) {
+        if (answers[i] === patterns[0][i % patterns[0].length]) scores[0]++;
+        if (answers[i] === patterns[1][i % patterns[1].length]) scores[1]++;
+        if (answers[i] === patterns[2][i % patterns[2].length]) scores[2]++;
+    }
+
+    const maxScore = Math.max(...scores);
+
+    for (let j = 0; j < scores.length; j++) {
+        if (scores[j] === maxScore) {
+            answer.push(j + 1);
         }
     }
-    
-    const maxScore = Math.max(...answerCount);
-    
-    for(let i = 0; i < answerCount.length; i++){
-        if(answerCount[i] === maxScore){
-            answer.push(i + 1);
-        }
-    }
-    
-    
-    
+
     return answer;
 }
