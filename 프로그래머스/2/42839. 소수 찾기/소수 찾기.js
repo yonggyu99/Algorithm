926da70efca92e8e9b748d6689920e42a3c59a45
yonggyu <yoyo5411@naver.com>
2026-04-10 16:04:03 +0900

    [level 2] Title: 소수 찾기, Time: 6.77 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -1,34 +1,35 @@
 function solution(numbers) {
-    
     const answer = new Set();
-    const nums = numbers.split('');
-    
-    function getAllNumbers(current, remaining){
-        if(current.length > 0){
-            answer.add(Number(current));
-        }
-        
-        for(let i = 0; i < remaining.length; i++){
-            getAllNumbers(
-            current + remaining[i],
-            remaining.filter((_, idx) => idx !== i)
-            );
-        }
-    }
-    getAllNumbers("", nums);
+    const num = numbers.split('');
     
     function isPrime(num){
         if(num < 2) return false;
-        for(let i = 2; i<= Math.sqrt(num); i++){
+        
+        for(let i = 2; i <= Math.sqrt(num); i++){
             if(num % i === 0) return false;
         }
+        
         return true;
     }
     
-    let count = 0;
+    function getAllNumbers(curr, remain){
+        if(curr.length > 0){
+            answer.add(Number(curr));
+        }
+        
+        for(let i = 0; i < remain.length; i++){
+            getAllNumbers(curr + remain[i],
+                         remain.filter((_, idx) => idx !== i)
+                          )
+            }
+        }
+    
+    getAllNumbers("", num);
     
     for(const num of answer){
-        if(isPrime(num)) count++;
+        if(!isPrime(num)) answer.delete(num);
     }
-    return count;
+    
+    
+    return answer.size;
 }
