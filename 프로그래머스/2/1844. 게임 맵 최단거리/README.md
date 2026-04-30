61737a8e435c7f9b4650a0ca6849747e9f11d520
yonggyu <yoyo5411@naver.com>
2026-04-30 10:22:17 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 33.59 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=javascript) 
 
 ### 성능 요약
 
-메모리: 37.5 MB, 시간: 30.87 ms
+메모리: 37.9 MB, 시간: 33.59 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 18일 21:48:21
+2026년 04월 30일 10:23:59
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,39 +1,31 @@
 function solution(maps) {
-  const n = maps.length;
-  const m = maps[0].length;
-
-  const dist = Array.from({ length: n }, () => Array(m).fill(0));
-
-  const dr = [-1, 1, 0, 0];
-  const dc = [0, 0, -1, 1];
-
-  const queue = [];
-  let head = 0;
-
-  if (maps[0][0] === 0) return -1;
-
-  dist[0][0] = 1;
-  queue.push([0, 0]);
-
-  while (head < queue.length) {
-    const [r, c] = queue[head++];
-
-    if (r === n - 1 && c === m - 1) return dist[r][c];
-
-    for (let k = 0; k < 4; k++) {
-      const nr = r + dr[k];
-      const nc = c + dc[k];
-
-      if (nr < 0 || nr >= n || nc < 0 || nc >= m) continue;
-
-      if (maps[nr][nc] === 0) continue;
-
-      if (dist[nr][nc] !== 0) continue;
-
-      dist[nr][nc] = dist[r][c] + 1;
-      queue.push([nr, nc]);
+    
+    const dx = [-1, 1, 0, 0];
+    const dy = [0, 0, -1, 1];
+    
+    const n = maps.length;
+    const m = maps[0].length;
+    
+    const queue = [[0, 0, 1]];
+    
+    maps[0][0] = 0;
+    
+    while(queue.length > 0){
+        const[x, y, dist] = queue.shift();
+        
+        if(x === n - 1 && y === m - 1) return dist;
+        
+        for(let i = 0; i < 4; i++){
+            const nx = x + dx[i];
+            const ny = y + dy[i];
+            
+            if(nx >= 0 && nx < n && ny >= 0 && ny < m && maps[nx][ny] === 1){
+                maps[nx][ny] = 0;
+                queue.push([nx, ny, dist + 1]);
+            }
+        }
     }
-  }
-
-  return -1;
+    
+    
+    return -1;
 }
