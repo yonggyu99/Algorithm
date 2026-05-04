e26aa148b7b7230b9bdaeae26379cdc485a9fc88
yonggyu <yoyo5411@naver.com>
2026-05-04 15:37:49 +0900

    [level 1] Title: 폰켓몬, Time: 1.13 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,14 +1,24 @@
 function solution(nums) {
     var answer = 0;
     
-    const canGet = nums.length / 2;
-    let numsType = {};
+    const type = new Set();
+    const size = nums.length / 2;
     
-    for(const type of nums){
-        numsType[type] = (numsType[type] || 0) + 1;
+    for(let i = 0; i < nums.length; i++){
+        type.add(nums[i]);
     }
     
-    const length = Object.keys(numsType).length;
+    if(type.size < size){
+        return type.size;
+    }else return size;
     
-    return canGet > length ? length : canGet;
+    
+    return answer;
 }
+
+//만약에 종류가 > 전체 /2 라면 => 가짓수가 되어야 하고?
+//만약에 < 라면 전체 / 2가 되면 된다.
+
+
+//[1,2,3,4,5,6]
+//종류는 6,  /2 는 3, 최대한 많은 종류를 가지고 가고 싶음.
