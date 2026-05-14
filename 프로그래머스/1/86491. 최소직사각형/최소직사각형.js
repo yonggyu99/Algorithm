f21c7a487d8ff5d3f7f0d5563c8122b66af03e84
yonggyu <yoyo5411@naver.com>
2026-05-14 18:29:58 +0900

    [level 1] Title: 최소직사각형, Time: 5.08 ms, Memory: 40.4 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,17 +1,15 @@
 function solution(sizes) {
-    var answer = 0;
     
-    let maxW = 0;
-    let maxH = 0;
-    
-    for(const card of sizes){
-        card.sort((a, b) => b - a);
+    for(const size of sizes){
+        size.sort((a , b) => b - a);
     }
     
-    for(let i = 0; i < sizes.length; i++){
-        if(maxW < sizes[i][0]) maxW = sizes[i][0];
-        if(maxH < sizes[i][1]) maxH = sizes[i][1];
-    }
+    let maxWidth = 0;
+    let maxHeight = 0;
     
-    return maxW * maxH;
+    for(let i = 0; i < sizes.length; i ++){
+    if(maxWidth < sizes[i][0]) maxWidth = sizes[i][0];
+    if(maxHeight < sizes[i][1]) maxHeight = sizes[i][1];
+        }
+    return maxWidth * maxHeight;
 }
